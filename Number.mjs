b3c6b8035ac9c86ce6e1ffb481df1ed62a25508f
Yaroslav <yaroslav@MacBook-Pro-Olga.local>
2023-03-18 16:03:53 +0300

    chanjes in Number

--- Number.mjs
+++ Number.mjs
@@ -2,7 +2,7 @@
 /* 1. создал переменную в которую закинул текст и импортировал
  */
 
-// let words = 'Secondary schools are usually much larger than primary schools and most children - over 80 percent - go to a comprehensive school at the age of 11. These schools are for all. Pupils do not need (to pass an exam to go to these schools. These schools are large. They have from 1.200 - 2.500 pupils. School lasts all day in the UK, so there is only one shift. In some areas with semi-detachement are there are grammar schools. Pupils must pass special exams to go to these schools.'
+// let words = ' jhcbxc Secondary schools are usually much larger than primary schools and most children - over 80 percent - go to a comprehensive school at the age of 11. These schools are for all. Pupils do not need (to pass an exam to go to these schools. These schools are large. They have from 1.200 - 2.500 pupils. School lasts all day in the UK, so there is only one shift. In some areas with semi-detachement are there are grammar schools. Pupils must pass special exams to go to these schools.'
 
 import words from './words.mjs'
 
