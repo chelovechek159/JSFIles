3bb2f73a0204006505df1acd056d5e37cceffbd6
Yaroslav <yaroslav@MacBook-Pro-Olga.local>
2023-03-18 16:33:17 +0300

    upgraded files

--- NewNumber.mjs
+++ NewNumber.mjs
@@ -1,22 +1,18 @@
 
 let text = 'Secondary schools are usually much larger than primary schools and most children - over 80 percent - go to a comprehensive school at the age of 11. These schools are for all. Pupils do not need (to pass an exam to go to these schools. These schools are large. They have from 1.200 - 2.500 pupils. School lasts all day in the UK, so there is only one shift. In some areas with semi-detachement are there are grammar schools. Pupils must pass special exams to go to these schools.'
 
-let arr = text
+text = text.replace(/[^\w\s\-]/g, '')
 
-arr = arr.replace(/[^\w\s\-]/g, '')
+let arr = text.split(' ').filter((n) => {return n != '-'})
 
-let newArr = arr.split(' ')
+text = arr.join(' ')
+//удаляю пробелы
+arr = text.replace(/\s+/g, ' ')
 
-newArr = newArr.filter((n) => {return n != '-'}).join(' ')
-
-// newArr = newArr.join(' ')
-
-newArr = newArr.replace(/\s+/g, ' ')
-
-let newWord = newArr.split(' ')
+let newWord = arr.split(' ')
 
 console.log(newWord)
 
-let numb = prompt('какое слово под номером?', newArr.length-1)
+let numb = prompt('какое слово под номером?', arr.length-1)
 alert('под номером ' + numb + ' слово ' + newWord[numb-1])
 
--- Number.mjs
+++ Number.mjs
@@ -1,24 +1,10 @@
 
 /* 1. создал переменную в которую закинул текст и импортировал
  */
-
-// let words = ' jhcbxc Secondary schools are usually much larger than primary schools and most children - over 80 percent - go to a comprehensive school at the age of 11. These schools are for all. Pupils do not need (to pass an exam to go to these schools. These schools are large. They have from 1.200 - 2.500 pupils. School lasts all day in the UK, so there is only one shift. In some areas with semi-detachement are there are grammar schools. Pupils must pass special exams to go to these schools.'
-
 import words from './words.mjs'
+let text = words
+// 2. удаляю знаки препинания кроме тире 3.убираю отдельные тире, чтобы оставить двойные слова 4. удаляю двойные пробелы4. 5.разбиваю текст на слова
 
-let arr = words
-// 2. удаляю знаки препинания кроме тире
+text = words.replace(/[^\w\s\-]/g, '').split(' ').filter((n) => {return n != '-'}).join(' ').replace(/\s+/g, ' ').split(' ')
 
-arr = arr.replace(/[^\w\s\-]/g, '')
-let word = arr.split(' ')
-// убираю отдельные тире, чтобы оставить двойные слова
-word = word.filter((n) => {return n != '-'})
-// 3. удаляю двойные пробелы
-word = word.join(' ').replace(/\s+/g, ' ')
-
-
-// 4. разбиваю текст на слова
-let newWord = word.split(' ')
-
-console.log(newWord)// ввожу нужный номер слова
-
+console.log(text)// ввожу нужный номер слова
